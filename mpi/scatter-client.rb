a9104efbae28d242b324a289b048fe832abc4932
Kaushik Shivakumar <kaushiks@berkeley.edu>
2020-12-29 20:40:27 -0800

    cleaning up scatter

--- mpi/scatter-client.rb
+++ mpi/scatter-client.rb
@@ -1,35 +1,18 @@
 require 'rubygems'
 require 'backports'
 require 'bud'
-require_relative 'scatter'
+require_relative 'scatter-test'
 
 class Client
   include Bud
-  include Scatter
+  include ScatterTest
 
   def initialize(server, sender, num_receivers, id, opts={})
     @server = server
-    @sender = sender.to_i
+    $sender = sender.to_i
     @num_receivers = num_receivers.to_i
-    @fn_ip_port = "127.0.0.1:1235"
-    @client_id = id.to_i
-    @data = [1, 2, 3, 4]
     puts "Initialized, client #" + @client_id.to_s
     super opts
-  end
-
-  def wait()
-    return true
-  end
-
-  def hello()
-    puts("HELLO")
-    return true
-  end
-
-  def ping(s)
-    puts(s)
-    return true
   end
 end
 
--- mpi/scatter.rb
+++ mpi/scatter.rb
@@ -1,18 +1,24 @@
 module Scatter
   state do
-    table :data_scatter
-    table :received_scatter
+    #table :data_scatter
+    #table :received_scatter
     table :ips_scatter
-    channel :message_func_scatter
+    table :ips_sender
+    table :ips_len
+    interface input, :data_scatter
+    interface output, :received_scatter
+    channel :message_func_scatter, [:@addr, :id] => [:val]
     periodic :timer, 1
+  end
 
-    data_scatter <+ [[0, 4.2], [1, 4.2], [2, 4.2], [3, 4.2]]
+  bootstrap do
     ips_scatter <+ [[0, "127.0.0.1:12346"], [1, "127.0.0.1:12347"], [2, "127.0.0.1:12348"]]
+    ips_sender <+ [[0, "127.0.0.1:12345"]]
   end
 
   bloom do
-      message_func_scatter <~ (ips_scatter * data_scatter * timer).combos {|i, d, t| [i.val, @data[d.key]] if (@sender > 0 and (d.key % @num_receivers) == i.key and ping(i.val))}
-      received_scatter <= message_func_scatter {|m| [m.val]}
-      stdio <~ message_func_scatter.inspected
+      ips_len <= ips_scatter.group([], count())
+      message_func_scatter <~ (ips_scatter * ips_len * data_scatter * ips_sender * timer).combos {|i, l, d, is, t| [i.val, d.key, d.val[(d.val.length()/l.key)*i.key..(d.val.length()/l.key)*(i.key + 1)-1]] if $sender > 0}
+      received_scatter <= message_func_scatter {|m| [m.id, m.val]}
   end
 end
